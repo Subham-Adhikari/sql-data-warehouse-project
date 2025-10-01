66b3cf2d010d1738b314c5bfd90624e972b47213
Subham Adhikari <165596073+Subham-Adhikari@users.noreply.github.com>
2025-10-01 17:02:28 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,10 +1,19 @@
 /*
 ===============================================================================
-Quality Checks - Silver Layer
+Quality Checks
 ===============================================================================
 Script Purpose:
-    Automated data quality checks for the Silver Layer tables after ETL. 
-    Ensures data consistency, accuracy, standardization, and proper relationships.
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
 
